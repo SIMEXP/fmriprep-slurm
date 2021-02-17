9d8425b1b836df03bb44a87b05c8930fe6b4f439
ltetrel <loic.tetrel.pro@gmail.com>
2021-02-17 13:44:13 -0500

    now copyting fmriprep derivative to sracth space, to avoid acl recursivity issues

--- fmriprep-slurm/main.py
+++ fmriprep-slurm/main.py
@@ -79,26 +79,17 @@ def load_bidsignore(bids_root):
     return tuple()
 
 
-def write_job_footer(fd, jobname, bids_path, fmriprep_workdir, derivatives_name, slurm_account):
+def write_job_footer(fd, jobname, bids_path, fmriprep_workdir, derivatives_name):
     fd.write("fmriprep_exitcode=$?\n")
-    user_derivative_dir = os.path.join(bids_path, "derivatives")
     local_derivative_dir = os.path.join("$SLURM_TMPDIR", os.path.basename(bids_path), "derivatives", derivatives_name)
-    # TODO: copy resource monitor output
     fd.write(
-        f"cp {fmriprep_workdir}/fmriprep_wf/resource_monitor.json /scratch/{os.environ['USER']}/{jobname}_resource_monitor.json \n"
+        f"if [ $fmriprep_exitcode -ne 0 ] ; then cp -R {fmriprep_workdir} $SCRATCH/{jobname}.workdir ; fi \n"
     )
     fd.write(
-        f"chgrp -R {slurm_account} {local_derivative_dir}\n"
+        f"cp {fmriprep_workdir}/fmriprep_wf/resource_monitor.json $SCRATCH/{jobname}_resource_monitor.json \n"
     )
     fd.write(
-        f"cp -R {local_derivative_dir} {user_derivative_dir}\n"
-    )
-    # user_derivative_dir = os.path.join("$SCRATCH", "derivatives", derivatives_name)
-    # fd.write(
-    #     f"cp -R {local_derivative_dir} {user_derivative_dir}\n"
-    # )
-    fd.write(
-        f"if [ $fmriprep_exitcode -ne 0 ] ; then cp -R {fmriprep_workdir} /scratch/{os.environ['USER']}/{jobname}.workdir ; fi \n"
+        f"cp -R {local_derivative_dir} $SCRATCH\n"
     )
     fd.write("exit $fmriprep_exitcode \n")
 
@@ -173,7 +164,7 @@ def write_fmriprep_job(layout, subject, args, anat_only=True):
             )
         )
         fmriprep_workdir = os.path.join("$SLURM_TMPDIR", "fmriprep_work")
-        write_job_footer(f, job_specs["jobname"], os.path.realpath(args.bids_path), fmriprep_workdir, args.derivatives_name, args.slurm_account)
+        write_job_footer(f, job_specs["jobname"], os.path.realpath(args.bids_path), fmriprep_workdir, args.derivatives_name)
     return job_path
 
 
@@ -314,7 +305,7 @@ def write_func_job(layout, subject, session, args):
             )
         )
         fmriprep_workdir = os.path.join("$SLURM_TMPDIR", "fmriprep_work")
-        write_job_footer(f, job_specs["jobname"], os.path.realpath(args.bids_path), fmriprep_workdir, args.derivatives_name, args.slurm_account)
+        write_job_footer(f, job_specs["jobname"], os.path.realpath(args.bids_path), fmriprep_workdir, args.derivatives_name)
 
     return job_path, outputs_exist
 
