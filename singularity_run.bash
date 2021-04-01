00342976df476309ee0b9c5ecbd358fba746d66f
ltetrel <loic.tetrel.pro@gmail.com>
2021-04-01 16:22:22 -0400

    adding directory creation for templateflow

--- singularity_run.bash
+++ singularity_run.bash
@@ -3,6 +3,7 @@
 PROJECT_PATH="/lustre03/project/6003287"
 DATASET_PATH=$1
 DATASET_FOLDER="${DATASET_PATH%/*}"
+TEMPLATEFLOW_DIR=/home/$USER/.cache/templateflow
 PYTHON_CMD="python /fmriprep-slurm/fmriprep-slurm/main.py "$@
 
 echo "Running fmriprep-slurm with singularity"
@@ -10,7 +11,10 @@ echo "singularity_run.bash "$@
 export SINGULARITYENV_TEMPLATEFLOW_HOME=/templateflow
 module load singularity/3.6
 
+echo "Creating templateflow directory in "$TEMPLATEFLOW_DIR
+mkdir -p $TEMPLATEFLOW_DIR
+
 echo $PYTHON_CMD | xargs singularity exec -B $PROJECT_PATH/fmriprep-slurm:/fmriprep-slurm \
   -B $DATASET_FOLDER:/DATA -B /etc/pki:/etc/pki \
-  -B /home/$USER/.cache/templateflow:/templateflow \
+  -B $TEMPLATEFLOW_DIR:/templateflow \
   $PROJECT_PATH/containers/fmriprep-20.2.1lts.sif
